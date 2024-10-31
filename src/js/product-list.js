436f8c8874ec97ed7ec88f82976a4fc262a0635c
PineappleLord2020 <mul20002@byui.edu>
2024-10-31 13:17:53 -0600

    I haven't committed this yet

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -2,6 +2,6 @@ import ProductList from "./components/ProductList.svelte";
 
 new ProductList({
     target: document.querySelector(".products"),
-    prop: { category: "tents" },
+    props: { category: "tents" },
   });
   renderHeaderFooter();
